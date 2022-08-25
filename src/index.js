d85be2ad31562f2873207adfbea4aea410e6fbb9
ThiagoCCR <thiago.ccr96@gmail.com>
2022-08-25 17:27:07 -0300

    feat GetTweets && PostTweets

--- src/index.js
+++ src/index.js
@@ -16,6 +16,23 @@ server.post('/sign-up', (req, res) => {
 
 });
 
+server.post('/tweets', (req, res) => {
+
+  tweets.push(req.body);
+
+  res.send('OK');
+
+});
+
+server.get('/tweets', (req, res) => {
+
+  const tweetsData = tweets.filter((val,index)=> index < 10) 
+
+  res.send(tweetsData);
+
+});
+
+
 server.listen(5000, () => {
   console.log('Listening on 5000');
 });
